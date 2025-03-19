f37dbd82bc03fcfb73b0b06ed03276f9bcf1c744
Joshua Park <josh.d.park@gmail.com>
2025-03-19 13:17:13 -0400

    htree2 implement next linked list for non-fast lookups
    This wasn't a rewrite from scratch, but merely me inplementing some
    parts that I needed to have a previous implementation for. But now that
    I know it works, I need to do a rewrite.
    
    The key discovery is that I can keep links to the symbols table from the
    fast lookup table.
    
    For example:
    
    symbol D has a code (.len=10) that cannot fit in our lookup table for fast
    decoding (can store up to 8 bits, that is codes with a length of up to
    8). So we take the prefix of symbol D (the first 8 bits) and store a
    flag so that we know that this needs a level of indirection. Then we
    store an index to the symbol tables.
    
    Since we may have > 1 code with that exact prefix, it would be best for
    us to also use a linked list method of prepending entries in the list.
    
    ```
    var p = &lookup[prefix];
    const temp = p.next;
    p.len = 0;
    p.next = symbol_idx;
    symbol[symbol_idx].next = temp;
    ```
    
    One thing that I've already thought about is that this linked list is
    prepending symbols in ascending order of code lengths. This means that
    during the decoding process, going off the happy path of the fast lookup
    table means traversing the linked list starting with the code with the
    most bits, which is the opposite of what we want. Huffman codes are
    based on symbol frequency statistics and so it is therefore more likely
    that the code we are looking for has fewer bits. So it would be better
    to build the lookup table from the end of the symbols table to the
    beginning.

--- src/htree2.zig
+++ src/htree2.zig
@@ -1,18 +1,34 @@
 const std = @import("std");
 
-fn htree(comptime alphabet_size: usize, comptime max_codelen: u4) type {
+fn htree(comptime alphabet_size: usize, comptime max_codelen: u4, comptime lookup_size: u4) type {
     return struct {
-        freq: [max_codelen + 1]u12 = [_]u12{0} ** (max_codelen + 1),
+        freq: [max_codelen + 1]u12 = @splat(0),
         next_code: [max_codelen + 1]Code = undefined,
         symbol: [alphabet_size]Codeword = undefined,
-        lookup: [1 << @bitSizeOf(@TypeOf(max_codelen))]Codeword = undefined,
+        lookup: [1 << lookup_size]FastLookup = @splat(FastLookup{ .len = 0, .payload = .{ .next = .nil } }),
 
         const Code = u15;
+
+        const CodewordIndex = enum(u12) {
+            nil = std.math.maxInt(u12),
+            _,
+        };
+
+        // can I make a comptime assertion that .nil > lookup.len?
+
+        const FastLookup = packed struct {
+            len: u4, // packed structs cannot have null, so I have to use 0
+            payload: packed union {
+                symbol: u12,
+                next: CodewordIndex,
+            },
+        };
 
         const Codeword = packed struct {
             code: Code,
             len: u4,
             symbol: u12,
+            next: CodewordIndex,
         };
 
         fn build(self: *@This(), codelens: []const u4) void {
@@ -31,26 +47,33 @@ fn htree(comptime alphabet_size: usize, comptime max_codelen: u4) type {
                 roff.* = loff + freq;
             // sort symbols canonically into symbols table
             for (0.., codelens) |i, len| {
-                const symbol: u8 = @intCast(i);
+                const symbol: u12 = @intCast(i);
                 code = self.next_code[len];
                 self.next_code[len] += 1;
-                self.symbol[offset[len]] = .{ .code = code, .len = len, .symbol = symbol };
+                self.symbol[offset[len]] = .{ .code = code, .len = len, .symbol = symbol, .next = .nil };
                 offset[len] += 1;
             }
             // create lookup table
-            for (&self.symbol) |*codeword| {
+            for (&self.symbol, 0..) |*codeword, symbol_idx| {
                 var reverse = @bitReverse(codeword.code) >> (@bitSizeOf(Code) - codeword.len);
+                // unable to do a fast lookup, use a linked list
+                if (codeword.len > lookup_size) {
+                    reverse &= (1 << lookup_size) - 1;
+                    const link = self.lookup[reverse].payload.next;
+                    self.lookup[reverse] = .{ .len = 0, .payload = .{ .next = @enumFromInt(symbol_idx) } };
+                    codeword.next = link;
+                    continue;
+                }
                 const stride: Code = @as(Code, 1) << codeword.len;
-                std.debug.print("{any}, reversed code: {b} \n", .{ codeword.*, reverse });
                 while (reverse < self.lookup.len) : (reverse += stride)
-                    self.lookup[reverse] = codeword.*;
+                    self.lookup[reverse] = .{ .len = codeword.len, .payload = .{ .symbol = codeword.symbol } };
             }
         }
     };
 }
 
 test htree {
-    const Htree = htree(8, 4);
+    const Htree = htree(8, 4, 3);
     // alphabet: A, B, C, D, E, F, G
     const codelengths = [_]u4{ 3, 3, 3, 3, 3, 2, 4, 4 };
     var decoder: Htree = .{};
@@ -58,12 +81,27 @@ test htree {
     // try std.testing.expectEqualSlices(u12, &.{ 0, 0, 1, 5, 2 }, &decoder.freq);
     // try std.testing.expectEqualSlices(u12, &decoder.next_code, &.{ 0, 0, 0, 2, 14 });
     try std.testing.expectEqualSlices(Htree.Code, &decoder.next_code, &.{ 0, 0, 1, 7, 16 });
-    try std.testing.expect(std.meta.eql(decoder.symbol[0], Htree.Codeword{ .len = 2, .code = 0, .symbol = 5 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[1], Htree.Codeword{ .len = 3, .code = 2, .symbol = 0 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[2], Htree.Codeword{ .len = 3, .code = 3, .symbol = 1 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[3], Htree.Codeword{ .len = 3, .code = 4, .symbol = 2 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[4], Htree.Codeword{ .len = 3, .code = 5, .symbol = 3 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[5], Htree.Codeword{ .len = 3, .code = 6, .symbol = 4 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[6], Htree.Codeword{ .len = 4, .code = 14, .symbol = 6 }));
-    try std.testing.expect(std.meta.eql(decoder.symbol[7], Htree.Codeword{ .len = 4, .code = 15, .symbol = 7 }));
+    // for (decoder.symbol) |codeword|
+    //     std.debug.print("{any}\n", .{codeword});
+    // symbol table tests
+    try std.testing.expect(std.meta.eql(decoder.symbol[0], Htree.Codeword{ .len = 2, .code = 0, .symbol = 5, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[1], Htree.Codeword{ .len = 3, .code = 2, .symbol = 0, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[2], Htree.Codeword{ .len = 3, .code = 3, .symbol = 1, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[3], Htree.Codeword{ .len = 3, .code = 4, .symbol = 2, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[4], Htree.Codeword{ .len = 3, .code = 5, .symbol = 3, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[5], Htree.Codeword{ .len = 3, .code = 6, .symbol = 4, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[6], Htree.Codeword{ .len = 4, .code = 14, .symbol = 6, .next = .nil }));
+    try std.testing.expect(std.meta.eql(decoder.symbol[7], Htree.Codeword{ .len = 4, .code = 15, .symbol = 7, .next = @enumFromInt(6) }));
+    // lookup tests
+    try std.testing.expect(std.meta.eql(decoder.lookup[0], Htree.FastLookup{ .len = 2, .payload = .{ .symbol = 5 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[1], Htree.FastLookup{ .len = 3, .payload = .{ .symbol = 2 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[2], Htree.FastLookup{ .len = 3, .payload = .{ .symbol = 0 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[3], Htree.FastLookup{ .len = 3, .payload = .{ .symbol = 4 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[4], Htree.FastLookup{ .len = 2, .payload = .{ .symbol = 5 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[5], Htree.FastLookup{ .len = 3, .payload = .{ .symbol = 3 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[6], Htree.FastLookup{ .len = 3, .payload = .{ .symbol = 1 } }));
+    try std.testing.expect(std.meta.eql(decoder.lookup[7], Htree.FastLookup{ .len = 0, .payload = .{ .symbol = 7 } }));
+    // for (decoder.lookup) |fast|
+    //     std.debug.print("len: {d}, next: {d}\n", .{ fast.len, if (fast.len == 0) @intFromEnum(fast.payload.next) else fast.payload.symbol });
+    try std.testing.expect(1 == 0);
 }
